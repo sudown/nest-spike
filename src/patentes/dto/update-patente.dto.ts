02089856ac5ea3249acdf525559ae55fa027e2ac
sudown <mirosmarsantoss@gmail.com>
2023-10-24 13:13:11 -0300

    Change PartialType import source 2 @nestjs/swagger

--- src/patentes/dto/update-patente.dto.ts
+++ src/patentes/dto/update-patente.dto.ts
@@ -1,12 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreatePatenteDto } from './create-patente.dto';
-import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdatePatenteDto extends PartialType(CreatePatenteDto) {
-  @ApiProperty({ required: false })
-  Nome?: string;
-  @ApiProperty({ required: false })
-  Imagem?: string;
-  @ApiProperty({ required: false })
-  Descricao?: string;
-}
+export class UpdatePatenteDto extends PartialType(CreatePatenteDto) {}
--- src/pessoas/dto/update-pessoa.dto.ts
+++ src/pessoas/dto/update-pessoa.dto.ts
@@ -1,16 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreatePessoaDto } from './create-pessoa.dto';
-import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdatePessoaDto extends PartialType(CreatePessoaDto) {
-  @ApiProperty({ required: false })
-  Nome?: string;
-  @ApiProperty({ required: false })
-  Email?: string;
-  @ApiProperty({ required: false })
-  Senha?: string;
-  @ApiProperty({ required: false })
-  Tipo?: string;
-  @ApiProperty({ required: false })
-  XP?: number;
-}
+export class UpdatePessoaDto extends PartialType(CreatePessoaDto) {}
