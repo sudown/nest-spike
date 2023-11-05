44903cc358b5f5054c91bc398087b0493bce07fb
sudown <mirosmarsantoss@gmail.com>
2023-11-05 16:33:13 -0300

    documented login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,48 @@
 import { Controller, Body, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from './guard/public.route';
+import {
+  ApiOperation,
+  ApiProperty,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
-type SignInRequest = {
+class SignInRequestDto {
+  @ApiProperty()
   Id: number;
+  @ApiProperty()
   Senha: string;
-};
+}
 
+class AuthResponseDto {
+  @ApiProperty()
+  access_token: string;
+}
+
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Public()
+  @ApiOperation({ summary: 'Autenticar usuário' })
+  @ApiResponse({
+    status: 200,
+    description: 'Usuário autenticado',
+    type: AuthResponseDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Credenciais inválidas',
+    content: {
+      'application/json': {
+        schema: { example: { message: 'Unauthorized', statusCode: 401 } },
+      },
+    },
+  })
   @Post('login')
-  signIn(@Body() body: SignInRequest) {
+  signIn(@Body() body: SignInRequestDto) {
     return this.authService.signIn(body.Id, body.Senha);
   }
 }
